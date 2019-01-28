d4968f1541001a13f55afee528def8983af92ae2
VitaliBrych333 <36643236+VitaliBrych333@users.noreply.github.com>
2019-01-28 03:24:53 +0300

    refactor: delete white spaces

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 $('.js-overlay-start').unbind('click').bind('click', function(e) {
-	e.preventDefault();
-	var src = $(this).attr('data-url');
-	$('.overlay-video').show();
-	setTimeout(function() {
-		$('.overlay-video').addClass('o1');
-		$('#player').attr('src', src);
-	}, 100);
+  e.preventDefault();
+  var src = $(this).attr('data-url');
+  $('.overlay-video').show();
+  setTimeout(function() {
+    $('.overlay-video').addClass('o1');
+    $('#player').attr('src', src);
+  }, 100);
 });
 
 $('.overlay-video').click(function(event) {
-	if (!$(event.target).closest('.videoWrapperExt').length) {
-		var PlayingVideoSrc = $('#player').attr('src').replace('&autoplay=1', '');
-		$('#player').attr('src', PlayingVideoSrc);
-		$('.overlay-video').removeClass('o1');
-		setTimeout(function() {
-			$('.overlay-video').hide();
-		}, 600);
-	}
+  if (!$(event.target).closest('.videoWrapperExt').length) {
+    var PlayingVideoSrc = $('#player').attr('src').replace('&autoplay=1', '');
+    $('#player').attr('src', PlayingVideoSrc);
+    $('.overlay-video').removeClass('o1');
+    setTimeout(function() {
+      $('.overlay-video').hide();
+    }, 600);
+  }
 });
 
 $('.close').click(function(event) {
-		var PlayingVideoSrc = $('#player').attr('src').replace('&autoplay=1', '');
-		$('#player').attr('src', PlayingVideoSrc);
-		$('.overlay-video').removeClass('o1');
-		setTimeout(function() {
-			$('.overlay-video').hide();
-		}, 600);
+  var PlayingVideoSrc = $('#player').attr('src').replace('&autoplay=1', '');
+  $('#player').attr('src', PlayingVideoSrc);
+  $('.overlay-video').removeClass('o1');
+  setTimeout(function() {
+    $('.overlay-video').hide();
+  }, 600);
 });
